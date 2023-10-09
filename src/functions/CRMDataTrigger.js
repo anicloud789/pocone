5de6a3ef541923ac1427f2ce01769826f345a13c
anicloud789 <kumar.anand.cloud2@gmail.com>
2023-10-09 19:40:25 +0530

    service queue trigger

---
+++ src/functions/CRMDataTrigger.js
@@ -0,0 +1,9 @@
+const { app } = require('@azure/functions');
+
+app.serviceBusQueue('CRMDataTrigger', {
+    connection: 'sbpocqueuedev_SERVICEBUS',
+    queueName: 'sb-poc-q-dev',
+    handler: (message, context) => {
+        context.log('Service bus queue function processed message:', message);
+    }
+});
